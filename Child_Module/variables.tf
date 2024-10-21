8700c502932fa4faba05fc7bc1e86cab07f8838a
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-10-21 14:28:27 +0530

    Update variables.tf

--- Child_Module/variables.tf
+++ Child_Module/variables.tf
@@ -16,17 +16,3 @@ variable "existing_vpc_ids" {
   description = "List of existing VPC IDs to import"
   type        = list(string)
 }
-
-
-
-#Updated variables.tf
-variable "imported_vpc_configs" {
-  description = "Imported VPC configurations"
-  type = map(object({
-    cidr_block           = string
-    enable_dns_support   = bool
-    enable_dns_hostnames = bool
-    tags                 = map(string)
-  }))
-  default = {}
-}
